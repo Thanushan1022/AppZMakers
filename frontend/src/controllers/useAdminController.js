9076f923e5857482535b7f895f4a61275858bace
Thanushan1022 <thanushan1022@gmail.com>
2026-06-17 20:22:34 +0530

    admin database

--- frontend/src/controllers/useAdminController.js
+++ frontend/src/controllers/useAdminController.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-const BACKEND_URL = 'https://appzmakers-production.up.railway.app /api';
+const BACKEND_URL = 'https://appzmakers-production.up.railway.app/api';
 
 export function useAdminController(adminId, updateAuth) {
   const [employees, setEmployees] = useState([]);
